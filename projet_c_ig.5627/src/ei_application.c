99463fa5d9c36375a5d1ff4c820a0aa8c6653faf
Antoine Moreau <antoine.moreau2@grenoble-inp.org>
2018-05-15 14:57:48 +0200

    creation fichiers C pour frame.c

---
+++ projet_c_ig.5627/src/ei_application.c
@@ -0,0 +1,29 @@
+#include "ei_application.h"
+
+void ei_app_create(ei_size_t* main_window_size, ei_bool_t fullscreen) {
+
+}
+
+void ei_app_free() {
+
+}
+
+void ei_app_run() {
+    getchar();
+}
+
+void ei_app_invalidate_rect(ei_rect_t* rect) {
+
+}
+
+void ei_app_quit_request() {
+
+}
+
+ei_widget_t* ei_app_root_widget() {
+    return NULL;
+}
+
+ei_surface_t ei_app_root_surface() {
+    return NULL;
+}
