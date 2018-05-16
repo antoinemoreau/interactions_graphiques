cec4a5804eb5eda0dc20cd5b4f2d0144018960ee
antoinemoreau <antoine.moreau.v@orange.fr>
2018-05-16 12:16:01 +0200

    on tente des choses pour la frame mon pote

--- projet_c_ig.5627/include/ei_frameclass.h
+++ projet_c_ig.5627/include/ei_frameclass.h
@@ -3,22 +3,27 @@
 
 #include "hw_interface.h"
 #include "ei_draw.h"
-#include "ei_widget.h"
-#include "ei_widgetclass.h"
+#include "ei_frame.h"
 
-typedef struct ei_frameclass_t {
-	ei_widget_t*		widget,
-	ei_size_t*			requested_size,
-	const ei_color_t*	color,
-	int*				border_width,
-	ei_relief_t*		relief,
-	char**				text,
-	ei_font_t*			text_font,
-	ei_color_t*			text_color,
-	ei_anchor_t*		text_anchor,
-	ei_surface_t*		img,
-	ei_rect_t**			img_rect,
-	ei_anchor_t*		img_anchor
-} ei_frameclass_t;
+struct ei_frameclass_t frame;
+
+frame.allocfunc = &ei_frameclass_allocfunc_t;
+frame.releasefunc = &ei_frameclass_releasefunc_t;
+frame.drawfunc = &ei_frameclass_drawfunc_t;
+frame.setdefaultsfunc = &ei_frameclass_setdefaultsfunc_t;
+frame.geomnotifyfunc = &ei_widgetclass_geomnotifyfunc_t;
+
+void* ei_frameclass_allocfunc_t ();
+
+void ei_frameclass_releasefunc_t (struct ei_frame_t* frame);
+
+void ei_frameclass_drawfunc_t (struct ei_frame_t*	frame,
+							 ei_surface_t		surface,
+							 ei_surface_t		pick_surface,
+							 ei_rect_t*		clipper);
+
+void ei_frameclass_setdefaultsfunc_t (struct ei_frame_t* frame);
+
+void ei_frameclass_geomnotifyfunc_t (struct ei_frame_t* frame, ei_rect_t rect);
 
 #endif
