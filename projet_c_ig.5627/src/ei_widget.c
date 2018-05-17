7d40011bcc93b058e5a5510a188eea96aa161e27
antoinemoreau <antoine.moreau.v@orange.fr>
2018-05-17 13:07:16 +0200

    maxime le plus beau*

--- projet_c_ig.5627/src/ei_widget.c
+++ projet_c_ig.5627/src/ei_widget.c
@@ -1,12 +1,33 @@
 #include "ei_widget.h"
+#include <stdlib.h>
 
 ei_widget_t*		ei_widget_create		(ei_widgetclass_name_t	class_name,
 							 ei_widget_t*		parent) {
 	ei_widgetclass_t* widgetclass = ei_widgetclass_from_name(class_name);
 	//On vérifie si la classe de ce widget existe
 	if (widgetclass) {
-		ei_widget_t* widget = *(widgetclass->allocfunc);
+		ei_widget_t* widget = widgetclass->allocfunc();
+		widget->wclass = widgetclass;
+
+		//id et color a modifier pour gestion des evenements
+		widget->pick_id = 1;
+		*(widget->pick_color) = ei_default_background_color;
+
+		//affectation du widget parent
+		widget->parent = parent;
+		widget->children_head = NULL;
+		widget->children_tail = NULL;
+
+		//a modifier lorsqu'on aura plusieurs widgets
+		widget->next_sibling = NULL;
+
+		//a modifier
+		widget->geom_params = NULL;
+
+		widgetclass->setdefaultsfunc(widget);
+		return widget;
 	}
+	return NULL;
 }
 
 void			ei_frame_configure		(ei_widget_t*		widget,
