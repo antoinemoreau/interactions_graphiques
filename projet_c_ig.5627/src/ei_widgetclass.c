cc4bd0db7d7cc055ffe7ecc644e2d037cc960faa
antoinemoreau <antoine.moreau.v@orange.fr>
2018-05-17 12:26:35 +0200

    fix des bugs de robin

--- projet_c_ig.5627/src/ei_widgetclass.c
+++ projet_c_ig.5627/src/ei_widgetclass.c
@@ -12,24 +12,23 @@ static ei_widgetclass_t* widgetclass_list = NULL;
 static ei_widgetclass_t* create_frame_widget(){
   ei_widgetclass_t *frame_widget = malloc(sizeof(ei_widgetclass_t));
   strncpy(frame_widget->name, "frameclass", 20);
-  frame_widget->allocfunc = (ei_widgetclass_allocfunc_t) &ei_frameclass_allocfunc;
-  frame_widget->releasefunc = (ei_widgetclass_releasefunc_t) &ei_frameclass_releasefunc;
-  frame_widget->drawfunc = (ei_widgetclass_drawfunc_t) &ei_frameclass_drawfunc;
-  frame_widget->setdefaultsfunc = (ei_widgetclass_setdefaultsfunc_t) &ei_frameclass_setdefaultsfunc;
-  frame_widget->geomnotifyfunc = (ei_widgetclass_geomnotifyfunc_t) &ei_frameclass_geomnotifyfunc;
+  frame_widget->allocfunc = (ei_widgetclass_allocfunc_t) &ei_frame_allocfunc;
+  frame_widget->releasefunc = (ei_widgetclass_releasefunc_t) &ei_frame_releasefunc;
+  frame_widget->drawfunc = (ei_widgetclass_drawfunc_t) &ei_frame_drawfunc;
+  frame_widget->setdefaultsfunc = (ei_widgetclass_setdefaultsfunc_t) &ei_frame_setdefaultsfunc;
+  frame_widget->geomnotifyfunc = (ei_widgetclass_geomnotifyfunc_t) &ei_frame_geomnotifyfunc;
   frame_widget->next = NULL;
   return frame_widget;
 }
 
-ei_widgetclass_t* ei_widgetclass_from_name (ei_widget_class_t name){
+ei_widgetclass_t* ei_widgetclass_from_name (ei_widgetclass_name_t name){
         for ( ei_widgetclass_t* current = widgetclass_list; current != NULL; current = current->next ) {
-                if (strcmp(ei_widgetclass_stringname(ei_current->name), ei_widgetclass_stringname(name)) == 0){
+                if (strcmp(ei_widgetclass_stringname(current->name), ei_widgetclass_stringname(name)) == 0){
                         return current;
                 }
         }
         return NULL;
 }
-
 
 void			ei_frame_register_class 	() {
   if (widgetclass_list == NULL) {
