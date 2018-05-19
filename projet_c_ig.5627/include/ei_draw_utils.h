a5845adcf217dbca4736d96e84e719ba36d9a776
LeTracteur <richardmaxime2@gmail.com>
2018-05-19 17:32:09 +0200

    g oublié lé fichié

---
+++ projet_c_ig.5627/include/ei_draw_utils.h
@@ -0,0 +1,17 @@
+#include "ei_draw.h"
+#include <stdio.h>
+
+/**
+        * calcul une nouvelle couleur à partir d'un couleur initiale
+        *
+        *@parameter initiale couleur que l'on veut modifier
+        *
+        *
+        *@parameter res couleur obtenu
+        *
+        *
+        *@ variation souahité
+        *
+        *
+*/
+void ei_compute_color(ei_color_t initiale, ei_color_t* res, float variation);
---
+++ projet_c_ig.5627/src/ei_draw_utils.c
@@ -0,0 +1,27 @@
+#include "ei_draw.h"
+#include <stdio.h>
+
+void ei_compute_color(ei_color_t initiale, ei_color_t* res, float variation){
+
+        fprintf(stdout, "%d, %f\n", initiale.red,variation);
+        int rouge = (int) initiale.red*variation ;
+        fprintf(stdout, "%d\n", rouge);
+        int bleu = (int) initiale.blue*variation;
+        int vert = (int) initiale.green*variation;
+        if (rouge < 255) {
+                res->red = rouge;
+        } else {
+                res->red = 255;
+        }
+        if (bleu < 255) {
+                res->blue = bleu;
+        } else {
+                res->blue = 255;
+        }
+        if (vert < 255) {
+                res->green = vert;
+        } else {
+                res->green = 255;
+        }
+        res->alpha = initiale.alpha;
+}
