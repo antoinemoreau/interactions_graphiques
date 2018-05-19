f3464d0edf7b72732b1bd9ae7b40eeffd0dc04a5
jeanner <robin.jeanne@grenoble-inp.org>
2018-05-19 18:32:53 +0200

    r

--- projet_c_ig.5627/include/ei_draw_utils.h
+++ projet_c_ig.5627/include/ei_draw_utils.h
@@ -15,3 +15,5 @@
         *
 */
 void ei_compute_color(ei_color_t initiale, ei_color_t* res, float variation);
+
+void ei_anchor_spot(ei_anchor_t anchor, ei_widget_t* widget, ei_point_t* anchor_position);
--- projet_c_ig.5627/src/ei_draw_utils.c
+++ projet_c_ig.5627/src/ei_draw_utils.c
@@ -22,3 +22,57 @@ void ei_compute_color(ei_color_t initiale, ei_color_t* res, float variation){
         }
         res->alpha = initiale.alpha;
 }
+
+
+void ei_anchor_spot(ei_anchor_t anchor, ei_widget_t* widget, ei_point_t* anchor_position){
+        /*
+        Renvoi la position du point choisi avec anchor_position
+
+        Condition : le widget a un geometry manager et anchor différent de none
+        */
+        if (strcmp(widget->geom_params->manager->name, "placer") != 0){
+                fprintf(stderr, "Geometry manager is not \"placer\" \n");
+                exit(1);
+        }
+        else if (anchor == ei_anc_none){
+                anchor = ei_anc_northwest;
+        }
+
+
+        if (anchor == ei_anc_center){
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width/2);
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height/2);
+        }
+        else if (anchor == ei_anc_north){
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width/2);
+                (*anchor_position).y = widget->screen_location->top_left.y;
+        }
+        else if (anchor == ei_anc_northeast){
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width);
+                (*anchor_position).y = widget->screen_location->top_left.y;
+        }
+        else if (anchor == ei_anc_east){
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width);
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height/2);
+        }
+        else if (anchor == ei_anc_southeast){
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width);
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height);
+        }
+        else if (anchor == ei_anc_south){
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height);
+                (*anchor_position).x = (int) (widget->screen_location->top_left.x +  widget->screen_location.size.width/2);
+        }
+        else if (anchor == ei_anc_southwest){
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height);
+                (*anchor_position).x = widget->screen_location->top_left.x;
+        }
+        else if (anchor == ei_anc_west){
+                (*anchor_position).x = widget->screen_location->top_left.x;
+                (*anchor_position).y = (int) (widget->screen_location->top_left.y +  widget->screen_location.size.height/2);
+        }
+        else if (anchor == ei_anc_northwest){
+                (*anchor_position).x = widget->screen_location->top_left.x;
+                (*anchor_position).y = widget->screen_location->top_left.y;
+        }
+}
