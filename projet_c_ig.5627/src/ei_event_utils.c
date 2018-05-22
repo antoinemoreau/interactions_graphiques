3b3c31e66ed8b9af31722cc4566211d807070158
Maxime Richard <maxime.richard2@grenoble-inp.org>
2018-05-22 14:15:54 +0200

    c'est la panique antoine revient nous vite

--- projet_c_ig.5627/src/ei_event_utils.c
+++ projet_c_ig.5627/src/ei_event_utils.c
@@ -1,2 +1,7 @@
 #include "ei_event_utils.h"
 #include "ei_event.h"
+
+extern ei_linked_event_t* listed_event;
+
+
+//faut un fonction pour def les events /bind de base, genre une init qu'on va appeller dans le ei_app_create
