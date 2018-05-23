b427c74fbe879bd11a151f0e83da21f10e700a46
Antoine Moreau <antoine.moreau2@grenoble-inp.org>
2018-05-23 11:22:30 +0200

    on fait des betises mdr

---
+++ projet_c_ig.5627/include/ei_application_utils.h
@@ -0,0 +1,14 @@
+#ifndef EI_APP_UTILS_H
+#define EI_APP_UTILS_H
+
+#include "ei_widget.h"
+#include <stdlib.h>
+
+/**
+ * Parcours en largeur récursif affichant tous les widgets
+ */
+void            draw_all_widgets        (ei_widget_t*   widget,
+                                         ei_surface_t   root_surface,
+                                         ei_surface_t   pick_surface);
+
+#endif
