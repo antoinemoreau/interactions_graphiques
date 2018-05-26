21c781dc9575a8e4cc256134debcba11e70355b3
antoinemoreau <antoine.moreau.v@orange.fr>
2018-05-27 00:03:19 +0200

    release en cours

--- projet_c_ig.5627/src/ei_widget.c
+++ projet_c_ig.5627/src/ei_widget.c
@@ -90,7 +90,8 @@ ei_widget_t*		ei_widget_create		(ei_widgetclass_name_t	class_name,
 
 
 void ei_widget_destroy (ei_widget_t* widget) {
-
+	//widget->geom_params->releasefunc()
+	free(widget);
 }
 
 ei_widget_t* ei_widget_pick (ei_point_t* where) {
