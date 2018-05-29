b24a977f93a4213ccfe90dd4136300da8c153ea2
antoinemoreau <antoine.moreau.v@orange.fr>
2018-05-30 00:47:46 +0200

    move to foreground en cours

--- projet_c_ig.5627/src/ei_event_utils.c
+++ projet_c_ig.5627/src/ei_event_utils.c
@@ -59,7 +59,7 @@ void ei_init_list_events (){
         ei_bind(ei_ev_mouse_buttonup, NULL, "toplevel", (ei_callback_t)&stop_resize, NULL);
 
         // Faire passer un widget au premier plan
-        //ei_bind(ei_ev_mouse_buttondown, NULL, "all", (ei_callback_t)&move_foreground, NULL);
+        ei_bind(ei_ev_mouse_buttondown, NULL, "all", (ei_callback_t)&move_foreground, NULL);
 }
 
 ei_linked_event_t* find_event(ei_widget_t* widget, ei_eventtype_t eventtype, ei_tag_t tag, ei_callback_t callback) {
@@ -182,7 +182,7 @@ ei_bool_t resizing_toplevel(ei_widget_t* widget, struct ei_event_t* event, void*
                 rect_list_add(rect_list, intersection1);
                 int diff_x = resized_widget->screen_location.size.width - new_size.x + event->param.mouse.where.x;
                 int diff_y = resized_widget->screen_location.size.height - new_size.y + event->param.mouse.where.y;
-                if(diff_x > resized_toplevel->min_size->width && diff_y > resized_toplevel->min_size->height){
+                if(diff_x > resized_toplevel->border_width && diff_y > 2 * resized_toplevel->border_width){
                         if (ei_axis_none) {
                                 resized_toplevel = NULL;
                         } else if(ei_axis_both) {
@@ -196,8 +196,7 @@ ei_bool_t resizing_toplevel(ei_widget_t* widget, struct ei_event_t* event, void*
                                 resized_widget->screen_location.size.height -= new_size.y - event->param.mouse.where.y;
                                 new_size = event->param.mouse.where;
                         }
-
-                        //ei_place(resized_widget, NULL, &(resized_widget->screen_location.top_left.x), &(resized_widget->screen_location.top_left.y), NULL, NULL, NULL, NULL, NULL, NULL);
+                        
                         ei_rect_t intersection2;
                         ei_intersection_rectangle(resized_widget->parent->content_rect ,&resized_widget->screen_location, &intersection2);
                         rect_list_add(rect_list, intersection2);
@@ -223,29 +222,37 @@ ei_bool_t stop_resize(ei_widget_t* widget, struct ei_event_t* event, void* user_
         return EI_FALSE;
 }
 
+static move_elt_to_end(ei_widget_t* widget) {
+
+}
+
 ei_bool_t move_foreground(ei_widget_t* widget, struct ei_event_t* event, void* user_param) {
         // On fait passer le widget et tous ses ancêtres à la fin de leur
         // listes de frères respectives
 
-        // ei_widget_t* previous = NULL;
-        // ei_bool_t is_toplevel = strcmp(widget->wclass->name, "toplevel") == 0);
+        ei_widget_t* previous = NULL;
+        ei_bool_t is_toplevel = strcmp(widget->wclass->name, "toplevel") == 0;
 
-        // if (!widget->parent) {
-        //         return EI_FALSE;
-        // }
+        if (!widget->parent) 
+                return EI_FALSE;
 
-        // // Si il est déjà en queue on passe directement au père
-        // if (!widget->next_sibling || (is_toplevel && widget->next_)
+        // Si il est déjà en queue on passe directement au père
+        if (!widget->next_sibling || (is_toplevel && !widget->next_sibling->next_sibling))
+                return move_foreground(widget->parent, event, user_param);
 
-        // // On passe par le parent et on itère sur le fils jusqu'a trouver le fils précédent
+        // Sinon on passe par le parent et on itère sur le fils jusqu'a trouver le fils précédent
+        previous = widget->parent->children_head;
+        if (previous != widget) {
+                while (previous->next_sibling != widget) {
+                        previous = previous->next_sibling;
+                }
+        }
         
-        // while()
+        // On décale le widget (ou les deux widgets dans le cas de la toplevel) à la fin de la liste
+        if (is_toplevel) {
 
-        // if (!widget->next_sibling) {
-        //         move_foreground(widget->parent, event, user_param);
-        // } else ( {
+        }
 
-        // }
-
-        
+        move_elt_to_end(widget);
 }
+
